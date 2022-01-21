e6928de00136a0cd6e9d07cd076c545fa9565382
IsaPolena <97200386+IsaPolena@users.noreply.github.com>
2022-01-20 17:46:18 -0800

    Added contributor names

--- filter-by-regex.py
+++ filter-by-regex.py
@@ -1,6 +1,6 @@
 # Initiated by Kelsey Kraus
 #
-# Contributors: <UPDATE ME!> 
+# Contributors: <Isabella Polena, Percy Garcia, Claire Cruse> 
 #
 # Description: <UPDATE ME!> This file currently contains the instructions for replicating the data cleaning method implemented by CTK 2016.
 
